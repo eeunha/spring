5553eadb7ec81dc646efe2b7e358f4a6a6122266
eeunha <eunha.dev@gmail.com>
2023-12-14 14:33:25 +0900

    231214 수업

--- ElasticsearchTest/src/main/java/com/test/controller/TestController.java
+++ ElasticsearchTest/src/main/java/com/test/controller/TestController.java
@@ -1,20 +1,28 @@
 package com.test.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.xcontent.XContentType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
@@ -22,7 +30,12 @@ public class TestController {
 	// 목록보기
 	@SuppressWarnings("deprecation") // 경고 노란 선 제거
 	@GetMapping(value = "/list.do")
-	public String list(Model model) {
+	public String list(Model model, String type, String word, String word2, @RequestParam(defaultValue = "0") int slop) {
+
+		if (type == null || type.equals("")) {
+			// 전체 검색
+			type = "match_all";
+		}
 
 		try {
 
@@ -38,7 +51,7 @@ public class TestController {
 			// 접근하려는 인덱스 참조 > spring 인덱스
 			SearchRequest searchRequest = new SearchRequest("spring");
 
-			SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().size(100);
+			SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().size(100); // 최대 100개의 결과물
 
 			// *** 엘라스틱서치 검색 기능
 			// GET spring/_search
@@ -47,23 +60,81 @@ public class TestController {
 			// "match_all": {}
 			// }
 			// }
-			searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+
+			if (type.equals("match_all")) {
+
+				// match all
+				searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+
+			} else if (type.equals("match_or")) {
+
+				// match
+				searchSourceBuilder.query(QueryBuilders.matchQuery("message", word));
+
+			} else if (type.equals("match_and")) {
+
+				// match + operator(and)
+				searchSourceBuilder.query(QueryBuilders.matchQuery("message", word).operator(Operator.AND));
+
+			} else if (type.equals("match_phrase")) {
+
+				// match phrase
+				if (slop == 0) {
+					searchSourceBuilder.query(QueryBuilders.matchPhraseQuery("message", word));
+				} else {
+					searchSourceBuilder.query(QueryBuilders.matchPhraseQuery("message", word).slop(slop));
+				}
+				
+			} else if (type.equals("match_bool")) {
+
+				// match bool
+				searchSourceBuilder.query(QueryBuilders.boolQuery()
+						.must(QueryBuilders.matchQuery("message", word)).mustNot(QueryBuilders.matchQuery("message", word2)));
+				
+			} else if (type.equals("match_should")) {
+
+				// match should
+				searchSourceBuilder.query(QueryBuilders.boolQuery()
+						.must(QueryBuilders.matchQuery("message", word))
+						.should(QueryBuilders.matchQuery("message", word)));
+				
+			}
+			
 
 			searchRequest.source(searchSourceBuilder);
 
-			
-			//실제 검색 요청
+			// 실제 검색 요청
 			SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-			
+
 			SearchHits searchHits = searchResponse.getHits();
-			
+
 			for (SearchHit hit : searchHits) {
+
 				System.out.println(hit);
+
 			}
-			
-			//사용 종료 > 엘라스틱서치 접속 종료
+
+			// js가 SearchHits 처리 못함
+			// SearchHits > (변환) > List<HashMap>
+
+			List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+			for (SearchHit hit : searchHits) {
+				// Document > HashMap
+				Map<String, Object> map = hit.getSourceAsMap();
+				map.put("id", hit.getId());
+				map.put("score", hit.getScore());
+				list.add(map);
+			}
+
+			model.addAttribute("list", list);
+			model.addAttribute("word", word);
+			model.addAttribute("type", type);
+			model.addAttribute("slop", slop);
+
+			// 사용 종료 > 엘라스틱서치 접속 종료
 			client.close();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -80,8 +151,34 @@ public class TestController {
 
 	// 추가하기(처리)
 	@PostMapping(value = "/addok.do")
-	public String addok(Model model) {
+	public String addok(Model model, String id, String message) {
 
-		return "addok";
+		try {
+
+			// client 객체
+			RestHighLevelClient client = new RestHighLevelClient(
+					RestClient.builder(new HttpHost("172.21.195.80", 9200, "http")));
+
+			/*
+			 * PUT spring/_doc/11 { "message": "안녕하세요. 홍길동입니다." }
+			 */
+
+			String data = String.format("{ \"message\": \"%s\" }", message);
+
+			IndexRequest indexRequest = new IndexRequest("spring").source(data, XContentType.JSON)
+					.setRefreshPolicy("wait_for");
+
+			indexRequest.id(id);
+
+			// 실제로 엘라스틱서치에 데이터(도큐먼트)를 추가하기
+			client.index(indexRequest, RequestOptions.DEFAULT);
+
+			client.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/list.do";
 	}
 }
--- ElasticsearchTest/src/main/webapp/WEB-INF/views/list.jsp
+++ ElasticsearchTest/src/main/webapp/WEB-INF/views/list.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,42 +8,158 @@
 <title>Example</title>
 <link rel="stylesheet" href="https://me2.do/5BvBFJ57">
 <style>
-	#list th:nth-child(1) {
-		width: 150px;	
-	}
-	#list th:nth-child(2) {
-		width: auto;	
-	}
-	#list th:nth-child(3) {
-		width: 120px;	
-	}
+#list th:nth-child(1) {
+	width: 150px;
+}
+
+#list th:nth-child(2) {
+	width: auto;
+}
+
+#list th:nth-child(3) {
+	width: 120px;
+}
+
+#list td:nth-child(1) {
+	text-align: center;
+}
+
+#list td:nth-child(3) {
+	text-align: center;
+}
+
+form {
+	border: 1px solid #CCC;
+	margin: 1rem;
+	padding-bottom: .5rem;
+}
+
+form div {
+	margin-bottom: 5px;
+}
 </style>
 </head>
 <body>
 	<!-- list.jsp -->
-	<h1>Elasticsearch <small>List</small></h1>
-	
+	<h1>
+		Elasticsearch <small>List</small>
+	</h1>
+
 	<div class="seperate">
-		<span></span>
-		<button type="button" class="add" onclick="location.href='/elasticsearch/add.do';">문서추가하기</button>
+		<button type="button" class="list"
+			onclick="location.href='/elasticsearch/list.do';">결과 초기화</button>
+		<button type="button" class="add"
+			onclick="location.href='/elasticsearch/add.do';">문서추가하기</button>
 	</div>
-	
+
 	<table id="list">
 		<tr>
 			<th>(문서)아이디</th>
 			<th>메시지</th>
 			<th>스코어</th>
 		</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>
+		<c:forEach items="${list}" var="map">
+			<tr>
+				<td>${map.id}</td>
+				<td>${map.message}</td>
+				<td>${map.score}</td>
+			</tr>
+		</c:forEach>
 	</table>
+
+	<!-- 검색 -->
+	<hr>
+
+	<form method="GET" action="/elasticsearch/list.do">
+		<div class="match_or">
+			<h4>검색하기 :: match(or)</h4>
+			<div>
+				<input type="text" name="word"> <input type="submit"
+					value="검색하기">
+			</div>
+		</div>
+		<input type="hidden" name="type" value="match_or">
+	</form>
+
+
+	<form method="GET" action="/elasticsearch/list.do">
+		<div class="match_and">
+			<h4>검색하기 :: match(and)</h4>
+			<div>
+				<input type="text" name="word"> <input type="submit"
+					value="검색하기">
+			</div>
+		</div>
+		<input type="hidden" name="type" value="match_and">
+	</form>
+
+
+	<form method="GET" action="/elasticsearch/list.do">
+		<div class="match_phrase">
+			<h4>검색하기 :: match(phrase)</h4>
+			<div>
+				<input type="text" name="word"> <input type="submit"
+					value="검색하기">
+			</div>
+			<div>
+				<div class="group">
+					<label>slop</label> <input type="number" name="slop" min="0"
+						value="0">
+				</div>
+			</div>
+		</div>
+		<input type="hidden" name="type" value="match_phrase">
+	</form>
+
+
+	<form method="GET" action="/elasticsearch/list.do">
+		<div class="match_bool">
+			<h4>검색하기 :: match_bool(must, must not)</h4>
+			<div class="group">
+				<label>포함</label> 
+				<input type="text" name="word">
+			</div>
+			<div class="group">
+				<label>미포함</label> 
+				<input type="text" name="word2">
+			</div>
+			<input type="submit" value="검색하기">
+		</div>
+
+		<input type="hidden" name="type" value="match_bool">
+	</form>
 	
+	
+	<form method="GET" action="/elasticsearch/list.do">
+		<div class="match_should">
+			<h4>검색하기 :: match_bool(should)</h4>
+			<div>
+				<input type="text" name="word"> 
+				<input type="submit" value="검색하기">
+			</div>
+		</div>
+		<input type="hidden" name="type" value="match_should">
+	</form>
+
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script>
+	
+		<c:if test="${type == 'match_all'}">
+			//전체 목록 보기
+			
+		</c:if>
+			
+		<c:if test="${type != 'match_all'}">
 		
+			$('form').hide();
+			$('.${type}').parent().show();
+		
+			//검색 결과
+			$('.${type} input[name=word]').val('${word}');
+			$('.${type} input[name=slop]').val('${slop}');s
+			
+		</c:if>
+			
 	</script>
 </body>
 </html>
